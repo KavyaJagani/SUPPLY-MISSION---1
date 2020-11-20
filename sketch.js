20a756408ab5f8d374493b8dd769d5d367c464a0
KavyaJagani <73023806+KavyaJagani@users.noreply.github.com>
2020-11-20 16:59:49 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
 var packageBody,ground
+var bg
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -13,55 +14,56 @@ function preload()
 }
 
 function setup() {
+
 	createCanvas(800, 700);
 	rectMode(CENTER);
-
-	background(0,0,0)
 	
-	package=createSprite(width/2, 100, 10,10);
-	package.addImage(packageIMG)
-	package.scale=0.2
 
-	helicopter=createSprite(width/2, 100, 10,10);
-	helicopter.addImage(helicopterIMG)
-	helicopter.scale=0.6
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
 
-	ground=createSprite(width/2, height-35, width,10);
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
 
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
 
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 220 , 5 , {restitution:1.0,static:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.8, isStatic:true});
 	World.add(world, packageBody);
+	var pos = packageBody.position;
+	var angle = packageBody.angle;
 	
-
-	
+	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
+	World.add(world, ground);
 
 
 	Engine.run(engine);
-  
 }
 
 
 function draw() {
   rectMode(CENTER);
   background(0);
-  
+  Engine.update(engine);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
   drawSprites();
- 
-}
+  show();
 
+}
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
-	package.x= packageBody.position.x 
-	package.y= packageBody.position.y 
+    Matter.Body.setStatic(packageBody, false);
   }
-
-  matter.packageBody.setStatic(false);
 }
 
-
-
+function show() {
+	
+	
+}
